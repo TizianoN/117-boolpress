88457aa34f45c747b82ee31c1f9836463053aeaf
TizianoN <hello@tizianonicolai.com>
2024-04-05 17:33:31 +0200

    add requests

--- app/Http/Requests/UpdatePostRequest.php
+++ app/Http/Requests/UpdatePostRequest.php
@@ -26,6 +26,7 @@ class UpdatePostRequest extends FormRequest
     return [
       'title' => 'required|string|max:255',
       'content' => 'required|string',
+      'category_id' => 'required|exists:categories,id',
     ];
   }
 
