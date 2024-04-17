54b77332864c9c7e9f862329698d3c563d037d8b
TizianoN <hello@tizianonicolai.com>
2024-04-17 09:08:16 +0200

    hopefully vite works

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
   |
   */
 
-  'url' => env('APP_URL', $_SERVER['SERVER_NAME']),
+  'url' => env('APP_URL', 'https://' . $_SERVER['SERVER_NAME']),
 
-  'asset_url' => env('ASSET_URL', $_SERVER['SERVER_NAME']),
+  'asset_url' => env('ASSET_URL', 'https://' . $_SERVER['SERVER_NAME']),
 
   /*
   |--------------------------------------------------------------------------
